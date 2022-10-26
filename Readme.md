d93ef896efeb65b2c8c0edc4f69083cf4ace496a
Lays Gazafi de Moraes <laysgazafi@gmail.com>
2022-10-26 13:31:09 -0300

    código do jogo completo

---
+++ Readme.md
@@ -0,0 +1,4 @@
+Jogo de corrida de carro
+
+Jogo simples em que os dois carros recebem a distância percorrida de forma aleatória, qual chegar primeiros aos 100 km vence.
+O objetivo do código é colocar em prática alguns conhecimentos de poo.
---
+++ corrida.py
@@ -0,0 +1,32 @@
+from random import randint 
+
+class Carro:
+
+    def __init__(self, marca, cor):
+        self.marca = marca
+        self.cor = cor
+        self.distancia = 0
+
+    def percorrer_distancia(self, distancia):
+        self.distancia += distancia
+
+def iniciar_jogo():
+    
+    ferrari = Carro('Ferrari', 'Vermelha')
+    porshe = Carro('Porshe', 'Preto')
+    
+    while True:
+        ferrari.percorrer_distancia(randint(0,10))
+        porshe.percorrer_distancia(randint(0,10))
+
+        if ferrari.distancia >= 100:
+            print(f'O vencedor foi o carro {ferrari.marca} {ferrari.cor}')
+            break
+
+        if porshe.distancia >= 100:
+            print(f'O vencedor foi o carro {porshe.marca} {porshe.cor}')
+            break
+
+iniciar_jogo()
+
+
